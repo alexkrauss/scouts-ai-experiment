d7da573b52bb0af71720f83471ed7e79e7e6146d
Alexander Krauss <alexander.krauss@qaware.de>
2024-11-29 17:28:20 +0100

    Enabled transaction management.

--- src/main/java/name/alexkrauss/scouts/ScoutsAiExperimentApplication.java
+++ src/main/java/name/alexkrauss/scouts/ScoutsAiExperimentApplication.java
@@ -3,9 +3,11 @@ package name.alexkrauss.scouts;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @OpenAPIDefinition
+@EnableTransactionManagement
 public class ScoutsAiExperimentApplication {
 
 	public static void main(String[] args) {
